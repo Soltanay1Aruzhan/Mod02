4b6e43362fb8f52062f2151148572f058722fb71
Soltanay1Aruzhan <123636392+Soltanay1Aruzhan@users.noreply.github.com>
2023-10-22 16:03:48 +0600

    Add files via upload

---
+++ Mod02Dz/Mod02Dz/Program.cs
@@ -0,0 +1,287 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mod02Dz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Task1");
+            int i = 0;
+            int s = 0;
+            string str = "";
+            string temp = "";
+            while (i == 0)
+            {
+                temp = Console.ReadLine();
+                if (temp.Equals("."))
+                {
+                    i = 1;
+                }
+                str = str + temp;
+            }
+            for (int j = 0; j < str.Length; j++)
+            {
+                if (str[j].Equals(' '))
+                {
+                    s++;
+                }
+            }
+            Console.WriteLine($"количество пробелов - {s}");
+
+            Console.WriteLine("Task2");
+            Console.WriteLine("введите счастливый билет:");
+            string temp = Console.ReadLine();
+            int b1 = Convert.ToInt32(temp[0] + temp[1] + temp[2]);
+            int b2 = Convert.ToInt32(temp[3] + temp[4] + temp[5]);
+            b1 = (b1 / 100) + (b1 % 100 / 10) + (b1 % 10);
+            b2 = (b2 / 100) + (b2 % 100 / 10) + (b2 % 10);
+            if (b1 == b2)
+            {
+                Console.WriteLine("True");
+            }
+            else
+            {
+                Console.WriteLine("False");
+            }
+
+            Console.WriteLine("Task3");
+            int A = 3;
+            int B = 7;
+            for (int i = A; i <= B; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write(i);
+                    Console.Write(" ");
+                }
+                Console.Write("\n");
+            }
+
+            Console.WriteLine("Task4");
+            string N = Console.ReadLine();
+            char[] C = N.ToCharArray();
+            Array.Reverse(C);
+            Console.WriteLine(C);
+
+            Console.WriteLine("Task5");
+            int x = Convert.ToInt32(Console.ReadLine());
+            int y = 7 * (x * x) - 3 * x + 6;
+            Console.WriteLine(y);
+
+            Console.WriteLine("Task6");
+            int a = Convert.ToInt32(Console.ReadLine());
+            x = 12 * (x * x) + 7 * a - 16;
+            Console.WriteLine(x);
+
+            Console.WriteLine("Task7");
+            Console.WriteLine("Введите а:");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Периметр равен {4 * a}");
+
+            Console.WriteLine("Task8");
+            Console.WriteLine("Введите r:");
+            int r = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Диаметр равен {2 * r}");
+
+            Console.WriteLine("Task9");
+            double R = Math.Sqrt(Math.Pow(6350.0 + 1.0, 2) - Math.Pow(6350.0, 2));
+            Console.WriteLine($"Расстояние до линии горизонта {R}");
+
+            Console.WriteLine("Task10");
+            Console.Write("Введите x:");
+            x = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите y:");
+            y = Convert.ToInt32(Console.ReadLine());
+            double z = x * 3 - 2.5 * x * y + 1.78 * (x * x) - 2.5 * y + 1;
+
+            Console.WriteLine("Task11");
+            Console.Write("Введите первое число:");
+            double h = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите второе число:");
+            double b = Convert.ToDouble(Console.ReadLine());
+            Console.Write($"среднее арифметическоe: {(h + b) / 2}");
+            Console.Write($"среднее геометрическое: {Math.Pow(h + b, 1.0 / 3)}");
+
+            Console.WriteLine("Task12");
+            Console.Write("Введите массу тела:");
+            double m = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите объем тела:");
+            double v = Convert.ToDouble(Console.ReadLine());
+            Console.Write($"Плотнасть тела равна: {m / v}");
+
+            Console.WriteLine("Task13");
+            int a = 1;
+            int b = 2;
+            int c = 3;
+            int Temp = b;
+            b = c;
+            c = a;
+            a = Temp;
+            Console.WriteLine($"A) a,b,c - {a}, {b}, {c}");
+            a = 1;
+            b = 2;
+            c = 3;
+            Temp = b;
+            b = a;
+            a = c;
+            c = Temp;
+            Console.WriteLine($"B) a,b,c - {a}, {b}, {c}");
+
+            Console.WriteLine("Task14");
+            bool X = false;
+            bool Y = true;
+            bool Z = false;
+            Console.WriteLine($" X или Z - {X || Z}");
+            Console.WriteLine($" X и Y - {X && Y}");
+            Console.WriteLine($" X и Z - {X && Z}");
+
+            Console.WriteLine("Task15");
+            double v1 = 15.4;
+            double v2 = 15.7;
+            if (v1 > v2)
+            {
+                Console.WriteLine("v1 больше v2");
+            }
+            else
+            {
+                Console.WriteLine("v2 больше v1");
+            }
+
+            Console.WriteLine("Task16");
+            double feet = 378;
+            double kilometr = 1.0;
+            if (feet * 0.0003048 > kilometr)
+            {
+                Console.WriteLine($"километр меньше чем {feet} футов");
+            }
+            else
+            {
+                Console.WriteLine($"{feet} футов меньше чем километр");
+            }
+
+            Console.WriteLine("Task17");
+            Console.Write("Введите натуральное число:");
+            int a = Convert.ToInt32(Console.ReadLine());
+            if (a % 2 == 1)
+            {
+                Console.WriteLine("число нечетное");
+            }
+            else
+            {
+                Console.WriteLine("число четное");
+            }
+            if (a % 10 == 7)
+            {
+                Console.WriteLine("число оканчивается на 7");
+            }
+            else
+            {
+                Console.WriteLine("число не оканчивается на 7");
+            }
+
+
+            Console.WriteLine("Task18");
+            Console.Write("Введите двухзначное число:");
+            int a = Convert.ToInt32(Console.ReadLine());
+            if (a / 10 > a % 10)
+            {
+                Console.WriteLine("первое число больше");
+            }
+            else if (a / 10 < a % 10)
+            {
+                Console.WriteLine("второе число больше");
+            }
+            else
+            {
+                Console.WriteLine("числа равны");
+            }
+
+            if (a / 10 == a % 10)
+            {
+                Console.WriteLine("цифры одинаковы");
+            }
+            else
+            {
+                Console.WriteLine("цифры не одинаковы");
+            }
+
+            Console.WriteLine("Task19");
+            Console.Write("Введите день недели: ");
+            int d = Convert.ToInt32(Console.ReadLine());
+            switch (d)
+            {
+                case 1:
+                    Console.WriteLine("Понелеьник");
+                    break;
+                case 2:
+                    Console.WriteLine("Вторник");
+                    break;
+                case 3:
+                    Console.WriteLine("Среда");
+                    break;
+                case 4:
+                    Console.WriteLine("Четверг");
+                    break;
+                case 5:
+                    Console.WriteLine("Пятница");
+                    break;
+                case 6:
+                    Console.WriteLine("Суббота");
+                    break;
+                case 7:
+                    Console.WriteLine("Воскресенье");
+                    break;
+            }
+
+            Console.WriteLine("Task20");
+            Console.Write("порядковый номер месяца: ");
+            int m = Convert.ToInt32(Console.ReadLine());
+            switch (m)
+            {
+                case 1:
+                    Console.WriteLine("Январь");
+                    break;
+                case 2:
+                    Console.WriteLine("Февраль");
+                    break;
+                case 3:
+                    Console.WriteLine("Март");
+                    break;
+                case 4:
+                    Console.WriteLine("Апрель");
+                    break;
+                case 5:
+                    Console.WriteLine("Май");
+                    break;
+                case 6:
+                    Console.WriteLine("Июнь");
+                    break;
+                case 7:
+                    Console.WriteLine("Июль");
+                    break;
+                case 8:
+                    Console.WriteLine("Август");
+                    break;
+                case 9:
+                    Console.WriteLine("Сентябрь");
+                    break;
+                case 10:
+                    Console.WriteLine("Октябрь");
+                    break;
+                case 11:
+                    Console.WriteLine("Ноябрь");
+                    break;
+                case 12:
+                    Console.WriteLine("Декабрь");
+                    break;
+            }
+        }
+    }
+}
---
+++ Mod02prak/Mod02prak/Program.cs
@@ -0,0 +1,316 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Mod02prak
+{
+    internal class Program
+    {
+
+        
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1  2  3");
+
+            Console.WriteLine("5 \n10 \n21");
+
+            Console.WriteLine((double)10/100);
+
+            Console.WriteLine(234/7);
+
+            Console.WriteLine("Task5");
+            int a = 99;
+            Console.WriteLine($"число десятков - {a/10}");
+            Console.WriteLine($"число единиц - {a % 10}");
+            Console.WriteLine($"сумма цифр - {(a / 10) + (a % 10)}");
+            Console.WriteLine($"произведение цифр - {(a / 10) * (a % 10)}");
+
+            Console.WriteLine("Task6");
+            bool A = true;
+            bool C = false;
+            bool B = false;
+            Console.WriteLine(A || B);
+            Console.WriteLine(A && B);
+            Console.WriteLine(B || C);
+
+            Console.WriteLine("Task7");
+            a = 20;
+            int r = 10;
+            if (a * a > 3.14 * r * r) 
+            {
+                Console.WriteLine("Площадь квадрата больше");
+            }
+            else
+            {
+                Console.WriteLine("Площадь круга больше");
+            }
+
+            Console.WriteLine("Task8");
+            int v1 = 23;
+            int m1 = 40;
+            int v2 = 21;
+            int m2 = 37;
+            if (m1 / v1 > m2 / v2)
+            {
+                Console.WriteLine("Плотность тела 1 больше");
+            }
+            else
+            {
+                Console.WriteLine("Плотность тела 2 больше");
+            }
+
+            Console.WriteLine("Task9");
+            int U1 = 30;
+            int U2 = 30;
+            int R1 = 4;
+            int R2 = 2;
+            if (U1 / R1 < U2 / R2)
+            {
+                Console.WriteLine("Сила тока меньше в 1 цепи");
+            }
+            else
+            {
+                Console.WriteLine("Сила тока меньше в 2 цепи");
+            }
+
+            Console.WriteLine("Task10");
+            for (int i = 20; i <= 35; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine("Task11");
+            Console.WriteLine("Введите b-");
+            int b = Convert.ToInt32(Console.ReadLine());
+            for (int i = 10; i <= b; i++)
+            {
+                Console.WriteLine(i * i);
+            }
+
+            Console.WriteLine("Task12");
+            Console.WriteLine("Введите a-");
+            a = Convert.ToInt32(Console.ReadLine());
+            for (int i = a; i <= 50; i++)
+            {
+                Console.WriteLine(i * i * i);
+            }
+
+            Console.WriteLine("Task13");
+            Console.WriteLine("Введите a-");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите b-");
+            b = Convert.ToInt32(Console.ReadLine());
+            if (b > a)
+            {
+                for (int i = a; i <= b; i++)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+            Console.WriteLine("Task14");
+            Console.WriteLine("Введите x-");
+            int x = Convert.ToInt32(Console.ReadLine());
+            int y = 3 * x ^ 2 + 2 * x + 4;
+            Console.WriteLine($"y = {y}");
+
+            Console.WriteLine("Task15");
+            Console.WriteLine("Введите радиус r-");
+            r = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Длина окружности - {2 * 3.14 * r} \n площадь круга - {3.14 * r * r}");
+
+            Console.WriteLine("Task16");
+            Console.WriteLine("Введите четырехзначное число: ");
+            int c = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Сумма цифр - {(c / 1000) + (c / 100 % 10) + (c / 10 % 10) + (c % 10)}");
+            Console.WriteLine($"произведение цифр - {(c / 1000) * (c / 100 % 10) * (c / 10 % 10) * (c % 10)}");
+
+            Console.WriteLine("Task17");
+            int r = 456;
+            Console.WriteLine($"1 - {r / 100}");
+            Console.WriteLine($"1 - {r % 100 / 10}");
+            Console.WriteLine($"1 - {r % 10}");
+            int x = 100 * (r / 100) + 10 * (r % 10) + (r % 100 / 10);
+            Console.WriteLine(x);
+
+            Console.WriteLine("Task18");
+            bool X = true;
+            bool Y = false;
+            Console.WriteLine(!X && !Y);
+            Console.WriteLine(X || (!X && Y));
+            Console.WriteLine((!X && Y) || Y);
+
+            Console.WriteLine("Task19");
+            int a = 3;
+            int b = 4;
+            int x = 4;
+            int y = 2;
+            swap(ref a, ref b);
+
+            Console.WriteLine("Task20");
+            f(ref a, ref b, x, y);
+            Console.WriteLine($"{a} \n {b}");
+
+            Console.WriteLine("Task21");
+            int m1 = 4;
+            int m2 = 2;
+            int m3 = 1;
+            if (m1 < m2 && m1 < m3)
+            {
+                Console.WriteLine($"наименьшее число {m1}");
+            }
+            else if (m2 < m1 && m2 < m3)
+            {
+                Console.WriteLine($"наименьшее число {m2}");
+            }
+            else
+            {
+                Console.WriteLine($"наименьшее число {m3}");
+            }
+
+            Console.WriteLine("Task22");
+            Random rnd = new Random();
+            int randomN = rnd.Next(2, 6);
+            switch (randomN)
+            {
+                case 2:
+                    Console.WriteLine("Неуд");
+                    break;
+                case 3:
+                    Console.WriteLine("Удовл");
+                    break;
+                case 4:
+                    Console.WriteLine("Хорош");
+                    break;
+                case 5:
+                    Console.WriteLine("Отлич");
+                    break;
+            }
+
+            Console.WriteLine("Task23");
+            int M = 10;
+            int N = 20;
+            Console.WriteLine(f(M, N));
+
+
+            int N = 8;
+            int n = 5;
+
+            int result = f(N, n);
+
+            Console.WriteLine($"Наименьшее число, большее или равное {N}, которое делится нацело на {n}, это {result}");
+
+
+        }
+        public static void swap(ref int a, ref int b)
+        {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+
+        static int f(int a, int b, int x, int y)
+        {
+            if (y < x)
+            {
+                a = x * y;
+                b = x / y;
+                return a;
+            }
+            else
+            {
+                a = y * x;
+                b = y / x;
+                return b;
+            }
+
+        }
+
+        static int f(int x, int y)
+        {
+            if (x == 0 && y == 0)
+            {
+                return 0;
+            }
+            else if (y == 0)
+            {
+                return 12 / x;
+            }
+            else if (x == 0)
+            {
+                return 12 / y;
+            }
+            else
+            {
+                return 144 / (x * y);
+            }
+        }
+
+        static int f(int h, int m, int s)
+        {
+            return h * 3600 + m * 60 + s;
+        }
+
+        static int f(int m, int d)
+        {
+            return m * 30 + d;
+        }
+
+        static int f(int m1, int m2, int m3)
+        {
+            if (m1 < m2 && m1 < m3)
+            {
+                return m1;
+            }
+            else if (m2 < m1 && m2 < m3)
+            {
+                return m2;
+            }
+            else
+            {
+                return m3;
+            }
+        }
+
+        static bool even(int n)
+        {
+            if (n % 2 == 1)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        static int f(int M, int N)
+        {
+            int s = 0;
+            for (int i = M; i <= N; i++)
+            {
+                if (i % 2 == 1)
+                {
+                    s += i;
+                }
+            }
+            return s;
+        }
+
+        static int f(int N, int n)
+        {
+            int result = N; 
+
+            while (result % n != 0)
+            {
+                result++; 
+            }
+
+            return result;
+        }
+    }
+}
